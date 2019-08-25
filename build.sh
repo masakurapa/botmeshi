5ac071912835dd6966521e8dbe387f39cdeced87
Masahiro Sakuraba <rabasan1031@gmail.com>
2019-08-25 13:03:51 +0900

    update deploy

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ export GOARCH=amd64
 go build -o built/event event/event.go
 go build -o built/interactive interactive/interactive.go
 go build -o built/invoke-search invoke_search/invoke_search.go
+
+cd built
+zip event.zip event
+zip interactive.zip interactive
+zip invoke-search.zip invoke-search
